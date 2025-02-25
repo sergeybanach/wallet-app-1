bbd00095ec5925e8ab8be8cb9912a241996bdfae
sergey <sergeybanach@gmail.com>
2025-02-25 07:04:36 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+// import { useState } from 'react'
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='bg-red-300'>
+        React app
+      </div>
+    </>
+  )
+}
+
+export default App
