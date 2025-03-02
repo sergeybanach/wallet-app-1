ffc341906a0054afa42151960f5ac7a339007a5a
sergey <sergeybanach@gmail.com>
2025-03-02 15:26:09 +0300

    bundle

--- bundle-files.js
+++ bundle-files.js
@@ -86,10 +86,8 @@ async function bundleProject() {
 
     console.log(`Found ${files.length} files. Bundling into ${outputFile}...`);
 
-    let bundledContent = "Debian 12, NodeJS 22.13.1, pnpm\n\n"
-
     // Prepare the bundled content
-    bundledContent += files
+    let bundledContent = files
       .map(file => `### ${file.filePath} ###\n${file.content.trimEnd()}\n`)
       .join('\n');
 
